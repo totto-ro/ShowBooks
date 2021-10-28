30de511758f368cc4892fdfd16006e3d15a41dcc
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-10-28 00:21:54 -0600

    First commit

---
+++ src/main/java/com/codingdojo/showbooks/ShowBooksApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.showbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingdojo/showbooks/ShowBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.showbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
